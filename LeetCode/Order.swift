9548f82bc0e110eb3d2be328a3b42b47776a7816
SebasMac <ionrodriguezlara@gmail.com>
2023-08-14 14:16:28 -0700

    First Commit

---
+++ LeetCode/Order.swift
@@ -0,0 +1,75 @@
+//
+//  Order.swift
+//  LeetCode
+//
+//  Created by Ion Sebastian Rodriguez Lara on 14/08/23.
+//
+
+import Foundation
+
+class Solution {
+    func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+        if !canFinish(numCourses, prerequisites) {
+            return []
+        }
+        var graph = [[Int]](repeating: [], count: numCourses)
+        var visited = [Bool](repeating: false, count: numCourses)
+        var result = [Int]()
+        for i in prerequisites {
+            graph[i[0]].append(i[1])
+        }
+        
+        func dfs(_ node: Int) {
+            visited[node] = true
+            for neighbor in graph[node] {
+                if !visited[neighbor] {
+                    dfs(neighbor)
+                }
+            }
+            result.append(node)
+        }
+        for node in 0..<graph.count {
+            if !visited[node] {
+                dfs(node)
+            }
+        }
+        return result.reversed()
+    }
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        var visited = [Bool](repeating: false, count: numCourses)
+        var graph = [[Int]](repeating: [], count: numCourses)
+        var temp = [Bool](repeating: false, count: numCourses)
+
+        for i in prerequisites {
+            graph[i[0]].append(i[1])
+        }
+
+        for node in 0..<numCourses {
+            if hasCycle(node, &visited, graph, &temp) {
+                return false
+            }
+        }
+        return true
+    }
+
+    func hasCycle(_ node: Int, _ visited: inout [Bool], _ graph: [[Int]], _ temp: inout [Bool]) -> Bool {
+        if !visited[node] {
+            visited[node] = true
+            temp[node] = true
+            for neighbor in graph[node] {
+                if neighbor == -1 {
+                    temp[node] = false
+                    return false
+                }
+                if !visited[neighbor] && hasCycle(neighbor, &visited, graph, &temp) {
+                    return true
+                } else if temp[neighbor] {
+                    return true
+                }
+            }
+            
+            temp[node] = false
+        }
+        return false
+    }
+}
---
+++ LeetCode/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Ion Sebastian Rodriguez Lara on 14/08/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
